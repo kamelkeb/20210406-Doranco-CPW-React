f9cadeec137b99e0171d1429938462a865dcdd73
berbiche <berbiche@gmail.com>
2021-04-09 12:20:58 +0200

    énoncé exo two way binding

---
+++ exoVendredi.js
@@ -0,0 +1,7 @@
+/*
+Exo: 
+Mettre en place sous le header (dans App.js), un champ de saisie pour
+récupérer le prénom de l'utilisateur.
+Afficher le prénom saisi dans le composant Header avec un petit message du type
+"Bonjour " + prénom
+*/
